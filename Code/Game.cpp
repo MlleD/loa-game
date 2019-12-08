80edfc1943396420ac7f10670c9f6066b59e4657
Dorothee Huynh <doroth3e.h@gmail.com>
2019-12-08 16:27:12 +0100

    Generation d'un plateau de jeu de dimension variable avec murs, porte, sol

--- Code/Game.cpp
+++ Code/Game.cpp
@@ -3,6 +3,8 @@
 
 #include "Wall.hpp"
 #include "Case.hpp"
+#include "Ground.hpp"
+#include "Door.hpp"
 
 typedef std::vector<Case> Board;
 
@@ -23,13 +25,35 @@ public:
 };
 
 
-Game::Game(int h, int w) : height(h), width(w), maxIndex(w*h)
+Game::Game(int h, int w) : height(h), width(w), maxIndex(w*h - 1)
 {
-    for (int i = 0; i < maxIndex; i++)
+    // Premiere ligne, celle du haut
+    for (int i = 0; i < width; i++)
     {
         Case c(new Wall());
         matrix.push_back(c);
     }
+    
+    // Lignes intermediaires
+    for (int i = 1; i < height - 1; i++)
+    {
+        matrix.push_back(new Wall());
+        for (int j = 1; j < width - 1; j++)
+        {
+            matrix.push_back(new Ground());
+        }
+        matrix.push_back(new Wall());
+    }
+
+    //Ligne du bas
+        for (int i = 0; i < width; i++)
+    {
+        Case c(new Wall());
+        matrix.push_back(c);
+    }
+
+    // Ajout d'une porte fermee, en bas a droite
+    matrix[maxIndex - 1] = Case(new Door());
 }
 
 void Game::print_game()
